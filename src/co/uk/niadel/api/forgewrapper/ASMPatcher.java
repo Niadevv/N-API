2a4e391f11da597ea3512cb115a1a665b7d20453
Niadel <niadel@virginmedia.com>
2014-05-29 16:08:16 +0100

    More ASM work, fixed the unobfed Item patching using obfed names.

--- src/co/uk/niadel/api/forgewrapper/ASMPatcher.java
+++ src/co/uk/niadel/api/forgewrapper/ASMPatcher.java
@@ -27,21 +27,40 @@ public class ASMPatcher implements IClassTransformer, Opcodes
 		}
 	}
 	
+	/**
+	 * Does the actual class patching. GOD BLESS YOU BYTECODE OUTLINE PLUGIN!
+	 * 
+	 * This only patches things that can't be Access Transformered or isn't already doable with Forge.
+	 * @param currClassName
+	 * @param newClassName
+	 * @param bytes
+	 * @return
+	 */
 	public byte[] patchClass(String currClassName, String newClassName, byte[] bytes)
 	{
 		ClassNode cn = new ClassNode();
 		ClassReader cr = new ClassReader(bytes);
 		ClassWriter cw = new ClassWriter(cr, ASM4);
+		FieldVisitor fv;
 		cr.accept(cn, 0);
 		
 		byte[] patchedBytes = null;
 		
 		switch (currClassName)
 		{
+			//Adding instance field so that the ItemBaseModArmour works
 			case "net.minecraft.item.Item":
-				cw.newField("abn", "instance", "public Labn;");
-				FieldVisitor fv = cw.visitField(ACC_PUBLIC, "instance", "public Labn", null, null);
+				cw.newField("net/minecraft/item/Item", "instance", "public Lnet/minecraft/item/Item;");
+				fv = cw.visitField(ACC_PUBLIC, "instance", "public Lnet/minecraft/item/Item", null, null);
 				fv.visitEnd();
+				break;
+				
+			//Obfuscated Item patching
+			case "abn":
+				cw.newField("abn", "instance", "public Labn;");
+				fv = cw.visitField(ACC_PUBLIC, "instance", "public Labn", null, null);
+				fv.visitEnd();
+				break;
 			
 			case "net.minecraft.client.renderer.entity.RenderItem":
 				MethodVisitor mv = cw.visitMethod(ACC_PUBLIC, "renderGameOverlay", "(FZII)V", null, null);
@@ -50,8 +69,8 @@ public class ASMPatcher implements IClassTransformer, Opcodes
 				mv.visitLabel(l261);
 				mv.visitLineNumber(498, l261);
 				mv.visitVarInsn(ALOAD, 0);
-				mv.visitMethodInsn(INVOKESTATIC, "co/uk/niadel/api/client/GUIHUDRegistry", "callAllRenderers", "(Lnet/minecraft/client/gui/GuiIngame;)V", false);
-
+				mv.visitMethodInsn(INVOKESTATIC, "co/uk/niadel/api/client/GUIHUDRegistry", "callAllRenderers", "(Lnet/minecraft/client/gui/GuiIngame;)V");
+				break;
 		}
 		
 		return bytes;
