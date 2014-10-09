25a78e5d34a4529184d90cf50809a66d1a8db434
Niadel <niadel@virginmedia.com>
2014-10-09 07:53:52 +0100

    Changed docs on MeasureRegistry.

--- src/co/uk/niadel/napi/measuresapi/MeasureRegistry.java
+++ src/co/uk/niadel/napi/measuresapi/MeasureRegistry.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Important if you want your mod to work with Forge mods. Currently not used internally.
+ * Currently only used internally. May be removed.
  */
 @NYI(firstPresence = "0.0")
 public class MeasureRegistry
