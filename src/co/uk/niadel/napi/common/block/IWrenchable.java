d308a7e8b5c7a9b8925f05915e571124ba426d9e
Niadel <niadel@virginmedia.com>
2014-10-09 08:00:34 +0100

    Documented IWrenchable.onWrench.

--- src/co/uk/niadel/napi/common/block/IWrenchable.java
+++ src/co/uk/niadel/napi/common/block/IWrenchable.java
@@ -10,5 +10,16 @@ import net.minecraft.world.World;
  */
 public interface IWrenchable
 {
+	/**
+	 * Called when this block is wrenched.
+	 * @param wrenchItem The item that was used to wrench this block
+	 * @param user The user who wrenched this block.
+	 * @param world The world object.
+	 * @param blockWrenched The block that has been wrenched.
+	 * @param x The block's x coord.
+	 * @param y The block's y coord.
+	 * @param z The block's z coord.
+	 * @param side The side wrenched. TODO replace this with an EnumFacing in 1.8
+	 */
 	public void onWrench(ItemStack wrenchItem, EntityPlayer user, World world, Block blockWrenched, int x, int y, int z, int side);
 }
